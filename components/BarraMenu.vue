32bc5aac0ace4f391d09ea8f2b4862fafb102514
lucas__almeida <lucaslap27@gmail.com>
2022-07-31 20:17:21 -0400

    Primeiro commit

---
+++ components/BarraMenu.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container mx-auto space-x-4 flex items-center h-10 mb-10">
+    <NuxtLink :to="{ name: 'a-empresa' }">Empresa</NuxtLink>
+    <NuxtLink :to="{ name: 'servicos' }">Serviços</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BarraMenu",
+};
+</script>
+
+<style>
+</style>
---
+++ pages/servicos.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="bg-gray-100">
+      <BarraMenu />
+    </div>
+
+    <div>
+      <div class="container mx-auto">
+        <h1 class="text-lg font-bold">Serviços</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia ea
+          velit placeat quibusdam corrupti, a maiores ut quidem laudantium
+          repellat recusandae architecto dolore iure, ad ex magni asperiores
+          ipsum. Laborum? Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Neque nulla nam laudantium blanditiis! Esse delectus laborum
+          beatae nobis commodi. Accusantium, molestias quas! Ducimus, inventore
+          modi molestias laborum eum sint beatae? Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Asperiores ipsum rerum optio quod,
+          doloremque obcaecati nesciunt exercitationem tempora dolores,
+          laboriosam eaque? Necessitatibus quibusdam beatae corporis veritatis
+          velit fuga voluptate recusandae!
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BarraMenu from "../components/BarraMenu.vue";
+export default {
+  name: "Servicos",
+  components: {
+    BarraMenu,
+  },
+};
+</script>
+
+<style>
+</style>
